b5510e5f41836275bd88f91d445ea35e4a02a700
Ruslana Semenenko <katotochkaaa@gmail.com>
2023-05-19 18:58:48 +0100

    homework

--- src/components/fetchImages/fetchImages.jsx
+++ src/components/fetchImages/fetchImages.jsx
@@ -4,6 +4,7 @@ const API_KEY = '36501043-81ad10453f0c142b16e03b30f';
 const BASE_URL = 'https://pixabay.com/api/';
 const PICS_ON_PAGE = 12;
 
+
 export async function fetchImages(inputData, page) {
   try {
     const response = await axios.get(BASE_URL, {
